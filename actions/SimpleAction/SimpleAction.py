907570b616e94bd7bfa407581ed3ddcdeb4d33d3
BuggeX <buggex@gmail.com>
2025-05-29 03:06:44 +0200

    some testing

--- actions/SimpleAction/SimpleAction.py
+++ actions/SimpleAction/SimpleAction.py
@@ -16,6 +16,38 @@ from gi.repository import Gtk, Adw
 class SimpleAction(ActionBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def get_config_rows(self) -> list:
+        self.sound_path = PathRow(self)
+
+        self.sound_title = Adw.EntryRow.new()
+        self.sound_title.set_title("Title")
+        self.sound_title.connect("changed", self.on_sound_title_changed)
+
+        self.load_config_values()
+
+        return [self.sound_path, self.sound_title]
+    
+    def on_sound_path_changed(self, sound_path):
+        settings = self.get_settings()
+        settings["sound_path"] = sound_path
+        self.set_settings(settings)
+
+    def on_sound_title_changed(self, sound_title):
+        settings = self.get_settings()
+        settings["sound_title"] = sound_title.get_text()
+        self.set_settings(settings)
+
+    def load_config_values(self):
+        settings = self.get_settings()
+        
+        path = settings.get("sound_path")
+        if path is not None:
+            self.sound_path.label.set_label(path)
+
+        title = settings.get("sound_title")
+        if title is not None:
+            self.sound_title.set_text(title)
         
     def on_ready(self) -> None:
         icon_path = os.path.join(self.plugin_base.PATH, "assets", "info.png")
@@ -26,3 +58,31 @@ class SimpleAction(ActionBase):
     
     def on_key_up(self) -> None:
         print("Key up")
+
+def on_path_choosen(dialog, result, pathrow):
+    file = dialog.open_finish(result)
+    path = file.get_path()
+    pathrow.label.set_label(path)
+    pathrow.action.on_sound_path_changed(path)
+
+class PathRow(Adw.PreferencesRow):
+    def __init__(self, simpleAction : SimpleAction):
+        super().__init__(title="Path:")
+        self.action = simpleAction
+
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, hexpand=True)
+        self.set_child(self.main_box)
+
+        self.label = Gtk.Label(hexpand=True, label="Path to sound file...", xalign=0, margin_start=12)
+        self.main_box.append(self.label)
+
+        self.config_button = Gtk.Button(label="Browse")
+        self.main_box.append(self.config_button)
+        self.config_button.connect("clicked", self.on_config)
+
+        self.dialog = Gtk.FileDialog.new()
+        self.dialog.set_title("Choose sound file")
+    
+    def on_config(self, button):
+        self.dialog.open(callback=on_path_choosen,user_data=self)
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.backend.PluginManager.ActionHolder import ActionHolder
 # Import actions
 from .actions.SimpleAction.SimpleAction import SimpleAction
 
-class PluginTemplate(PluginBase):
+class Soundbard(PluginBase):
     def __init__(self):
         super().__init__()
 
@@ -13,14 +13,14 @@ class PluginTemplate(PluginBase):
         self.simple_action_holder = ActionHolder(
             plugin_base = self,
             action_base = SimpleAction,
-            action_id = "dev_core447_Template::SimpleAction", # Change this to your own plugin id
+            action_id = "com_buggex_sc_soundboard::SimpleAction", # Change this to your own plugin id
             action_name = "Simple Action",
         )
         self.add_action_holder(self.simple_action_holder)
 
         # Register plugin
         self.register(
-            plugin_name = "Template",
+            plugin_name = "Soundbard",
             github_repo = "https://github.com/StreamController/PluginTemplate",
             plugin_version = "1.0.0",
             app_version = "1.1.1-alpha"
