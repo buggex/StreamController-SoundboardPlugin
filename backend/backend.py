eccd2c31a5506ff93334a94f9b064850ba0af304
BuggeX <buggex@gmail.com>
2025-06-02 02:08:35 +0200

    changed backend to vlc

--- backend/backend.py
+++ backend/backend.py
@@ -1,22 +1,43 @@
 from streamcontroller_plugin_tools import BackendBase
-from pathlib import Path
 
-import pygame
+import vlc
 
 class SoundboardBackend(BackendBase):
+    device      : str = ""
+    instance    : vlc.Instance = None
+    player      : vlc.MediaPlayer = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance = vlc.Instance()
 
     def set_device(self, device):
-        pygame.mixer.quit()
-        pygame.mixer.init(devicename=device)
+        self.device = device
 
-    def play_sound(self, path_to_sound, volume=100.0):
-        pygame.mixer.music.load(path_to_sound)
-        pygame.mixer.music.set_volume(volume/100.0)
-        pygame.mixer.music.play()
+    def play_sound(self, path_to_sound, volume=100):
+        # Stop old
+        self.stop_sound()
+
+        # Start new
+        media = self.instance.media_new("file://" + path_to_sound)
+        self.player = media.player_new_from_media()
+        self.player.audio_output_device_set(None, self.find_device(self.player, self.device))
+        self.player.audio_set_volume(int(volume)) # TODO why does volume not work
+        self.player.play()
 
     def stop_sound(self):
-        pygame.mixer.music.stop()
+        if self.player:
+            self.player.release()
+
+    def find_device(self, player, device_name):
+        devices = player.audio_output_device_enum()
+        if devices:
+            device = devices
+            while device:
+                device = device.contents
+                if device_name in str(device.description):
+                    return device.device
+                device = device.next
+        return None
 
 backend = SoundboardBackend()
